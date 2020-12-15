47a4fb0084e0be1f2d08cb67f192bd9cdb63882b
ivan3rd <74527889+ivan3rd@users.noreply.github.com>
2020-12-15 14:12:36 +0700

    Just uploaded one of the pages for practice
    Print out array of data and then used setState to re-render output.

---
+++ build/precache-manifest.cd3158ca5dae17df1b3caecb6396252c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e18703a5f72aceabacb18d62537e64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1dae3393e1d56ebf594",
+    "url": "/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1dae3393e1d56ebf594",
+    "url": "/static/js/main.d15d772e.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
