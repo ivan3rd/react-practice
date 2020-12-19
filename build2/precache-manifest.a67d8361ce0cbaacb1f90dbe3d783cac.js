49fa84e983ef98dfdf3ad427051a9f074f5c4d4e
ivan3rd <74527889+ivan3rd@users.noreply.github.com>
2020-12-19 17:05:28 +0700

    practiced with setState & delete or add buttons

---
+++ build2/precache-manifest.a67d8361ce0cbaacb1f90dbe3d783cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8ec75c80a16f6eef4e1508358299b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "392357381b30b67c42e9",
+    "url": "./static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "./static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392357381b30b67c42e9",
+    "url": "./static/js/main.48b8e2a3.chunk.js"
+  },
+  {
+    "revision": "634688380851e0148535",
+    "url": "./static/js/runtime-main.9a51be4d.js"
+  }
+]);
